b7ad01eb99557c502a4613224aeb5cf576ea196b
akiyoko <akiyoko@users.noreply.github.com>
2020-01-31 01:01:20 +0900

    Add get_absolute_url

--- shop/models.py
+++ shop/models.py
@@ -51,3 +51,7 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        from django.urls import reverse
+        return reverse('admin:shop_book_change', args=[self.id])
